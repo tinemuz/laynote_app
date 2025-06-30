357f60b54dd81516aafa5a670d08727797747233
tine <99731290+tinemuz@users.noreply.github.com>
2025-06-30 12:50:18 +0100

    Revert "base auth"
    This reverts commit c95bc58fd7f4345c98518d5647fad1f2fd82bd39.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,13 @@
-import { auth0 } from "@/lib/auth0";
-import './globals.css';
-import {Sidebar} from "@/components/sidebar/Sidebar";
 import {ProseMirrorEditor} from "@/components/editor/ProseMirrorEditor";
+import {Sidebar} from "@/components/sidebar/Sidebar";
 
-export default async function Home() {
-    // Fetch the user session
-    const session = await auth0.getSession();
-
-    // If no session, show sign-up and login buttons
-    if (!session) {
-        return (
-            <main>
-                <a href="/auth/login?screen_hint=signup">
-                    <button>Sign up</button>
-                </a>
-                <a href="/auth/login">
-                    <button>Log in</button>
-                </a>
-            </main>
-        );
-    }
-
-    // <main>
-    //     <h1>Welcome, {session.user.name}!</h1>
-    //     <p>
-    //         <a href="/auth/logout">
-    //             <button>Log out</button>
-    //         </a>
-    //     </p>
-    // </main>
-
-    // If session exists, show a welcome message and logout button
-    return (
-
-        <div className={"flex flex-row w-full"}>
-            <Sidebar/>
-            <div className={"flex-grow"}>
-                <ProseMirrorEditor/>
-            </div>
+export default function Home() {
+  return (
+      <div className={"flex flex-row w-full"}>
+        <Sidebar/>
+        <div className={"flex-grow"}>
+          <ProseMirrorEditor/>
         </div>
-
-    );
+      </div>
+  );
 }
